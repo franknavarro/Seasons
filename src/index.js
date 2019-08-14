6dd48a4ab2a4faac6cfc6feb1aacd956c737288c
Frank Navarro <fnavarro7777@gmail.com>
2019-08-14 12:08:41 -0700

    Got users location

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const App = () => {
+
+  // Get the users position
+  window.navigator.geolocation.getCurrentPosition(
+    (position) => console.log(position),
+    (err) => console.log(err)
+  );
+
   return <div>Hi there!</div>;
 };
 
