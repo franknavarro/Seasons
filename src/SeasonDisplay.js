8032a03fb927d9582842280d77adf4a4ba27cab0
Frank Navarro <fnavarro7777@gmail.com>
2019-08-17 22:37:32 -0700

    Added season text and icons

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,7 +1,35 @@
 import React from 'react';
 
-const SeasonDisplay = () => {
-  return <div>Season Display</div>;
+const seasonConfig = {
+  'summer':{
+    'description':"Dang. Its cold outside....",
+    'iconName':"sun"
+  },
+  'winter':{
+    'description':"Suns out guts out. Its HOT!",
+    'iconName':"snowflake"
+  }
+};
+
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? 'summer' : 'winter';
+  } else {
+    return lat > 0 ? 'winter' : 'summer';
+  }
+};
+
+const SeasonDisplay = (props) => {
+  const season = getSeason(props.lat, new Date().getMonth());
+  const { description, iconName } = seasonConfig[season]
+
+  return (
+    <div>
+      <i className={`icon ${iconName}`} />
+      <h1>{description}</h1>
+      <i className={`icon ${iconName}`} />
+    </div>
+  );
 };
 
 export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = { 
-      lat: null,
-      errorMessage: ''
-    };
-    
-  }
+  state = { 
+    lat: null,
+    errorMessage: ''
+  };
 
   componentDidMount() {
     // Get the users position
@@ -30,7 +26,7 @@ class App extends React.Component {
     }
 
     if (!this.state.errorMessage && this.state.lat) {
-      return <div> Latitude: {this.state.lat}</div>;
+      return <SeasonDisplay lat={this.state.lat} />;
     }
 
     return <div>Loading...</div>
