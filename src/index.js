f648a3976dd4190535a8d9c76bae12d62a585c1f
Frank Navarro <fnavarro7777@gmail.com>
2019-09-15 12:31:24 -0700

    Changed to functional component

--- src/index.js
+++ src/index.js
@@ -1,41 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 import Spinner from './Spinner';
 
-class App extends React.Component {
-  state = {
-    lat: null,
-    errorMessage: '',
-  };
+const App = () => {
+  const [lat, setLat] = useState(null);
+  const [err, setErr] = useState('');
 
-  componentDidMount() {
+  useEffect(() => {
     // Get the users position
     window.navigator.geolocation.getCurrentPosition(
       position => {
-        this.setState({ lat: position.coords.latitude });
+        setLat(position.coords.latitude);
       },
       err => {
-        this.setState({ errorMessage: err.message });
+        setErr(err.message);
       },
     );
+  }, []);
+
+  let content;
+  if (err) {
+    content = <div>Error: {err}</div>;
+  } else if (lat) {
+    content = <SeasonDisplay lat={lat} />;
+  } else {
+    content = <Spinner message="Please accept location request" />;
   }
 
-  renderContent() {
-    if (this.state.errorMessage && !this.state.lat) {
-      return <div>Error: {this.state.errorMessage}</div>;
-    }
-
-    if (!this.state.errorMessage && this.state.lat) {
-      return <SeasonDisplay lat={this.state.lat} />;
-    }
-
-    return <Spinner message="Please accept location request" />;
-  }
-
-  render() {
-    return <div>{this.renderContent()}</div>;
-  }
-}
+  return <div>{content}</div>;
+};
 
 ReactDOM.render(<App />, document.querySelector('#root'));
